16b63b70d97b92e243b255d31a7a520b5b52f975
Grzegorz Pasich <grzegorz.pasich@o2.pl>
2023-11-06 16:41:55 +0100

    structure changes

--- backend/server/products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import AddProductView, GetProductsView
-
-urlpatterns = [
-    path('add', AddProductView.as_view()),
-    path('get', GetProductsView.as_view()),
-]
